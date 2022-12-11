d25bb205f1f2b1298bba00d4459b0f8b1df28ad2
sheepness <edwardchen114@gmail.com>
2022-12-10 19:06:41 -0800

    seeding

--- peepee.js
+++ peepee.js
@@ -2,6 +2,8 @@ var guesses;
 var guesses2;
 var answers;
 var answer = "sheep";
+
+
 function editDistance(w1, w2) {
 	var dp = []; //edit distance between prefixes of i,j lengths
 	for (var i=0; i<=w1.length; i++) {
@@ -25,11 +27,23 @@ function editDistance(w1, w2) {
 	return dp[w1.length][w2.length];
 }
 
+function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+
 function init() {
 	guesses = JSON.parse(list);
 	guesses2 = JSON.parse(list2);
 	answers = JSON.parse(list3);
-	answer = answers.dictionary[Math.floor(Math.random()*answers.dictionary.length)];
+	//answer = answers.dictionary[Math.floor(Math.random()*answers.dictionary.length)];
+	var dt = new Date();
+	var xd = dt.getUTCFullYear()*400+dt.getUTCMonth()*40+dt.getUTCDate();
+	answer = answers.dictionary[mulberry32(xd)%answers.dictionary.length];
 }
 
 function guess(w) {
